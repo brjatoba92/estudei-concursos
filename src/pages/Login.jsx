f42d15ed482242de77a99df952f8ff4c3cc1e371
brjatoba92 <brjatoba92@gmail.com>
2025-05-06 17:39:42 -0300

    firebase, dashboard, Login and Register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [erro, setErro] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        } catch (error) {
+            setErro("Email ou senha invalidos");
+        }
+    };
+
     return (
         <div className="max-w-md mx-auto mt-10 mg-white p-8 rounded shadow">
             <h2 className="text-2xl font-bold mb-6 text-center text-blue-700">Página de Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                     <label className="block text-gray-700 mb-2">Email</label>
                     <input
                         type="email"
                         className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Digite seu email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
                     />
                 </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Senha</label>
+                    <input
+                        type="password"
+                        className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="Digite sua senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                {erro && <p className="text-red-600 text-sm mb-4">{erro}</p>}
                 <button
                     type="submit"
                     className="w-full bg-blue-700 text-white py-2 rounded hover:bg-blue-800"
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
 
 function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [erro, setErro] = useState("");
+    const navigate = useNavigate();
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            navigate("/");
+        } catch (error) {
+            setErro("Email ou senha invalidos");
+        }
+    };
+
     return (
         <div className="max-w-md mx-auto mt-10 bg-white p-8 rounded shadow">
             <h2 className="text-2xl font-bold mb-6 text-center text-blue-700">Cadastro</h2>
-            <form>
+            <form onSubmit={handleRegister}>
                 <div className="mb-4">
                     <label className="block text-gray-700 mb-2">
                         Nome
@@ -14,6 +31,9 @@ function Register() {
                         type="text"
                         className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Seu nome completo"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
                     />
                 </div>
                 <div className="mb-4">
@@ -24,6 +44,9 @@ function Register() {
                         type="email"
                         className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="Digite seu email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
                     />
                 </div>
                 <div className="mb-4">
@@ -34,17 +57,21 @@ function Register() {
                         type="password"
                         className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                         placeholder="*********"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
                     />
                 </div>
+                {erro && <p className="text-red-500 text-center">{erro}</p>}
                 <button
                     type="submit"
-                    className="w-full bg-blue-700 text-white py-2 rounded hover:bg-blue-800"
+                    className="w-full bg-green-700 text-white py-2 rounded hover:bg-green-800"
                 >
                     Criar conta
                 </button>
                 <p className="mt-4 text-center text-sm">
                     Já tem conta?{" "}
-                    <Link to="/login" className="text-blue-700 hover:underline">
+                    <Link to="/login" className="text-green-700 hover:underline mx-1">
                         Entrar
                     </Link>
                 </p>
