49190e1d383042e9aab0345fe1e0721991671e21
brjatoba92 <brjatoba92@gmail.com>
2025-05-07 12:37:34 -0300

    Button Logout Action Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Footer from "./components/Footer";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebaseConfig";
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-gray-50 text-gray-800">
-        <AppRoutes />
+        <AppRoutes user={user} />
         <Footer />
       </div>
     </Router>
