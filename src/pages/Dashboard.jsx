149035b7cd289e11f32358fd8f6f4da3af033c72
brjatoba92 <brjatoba92@gmail.com>
2025-05-06 22:27:00 -0300

    Topbar horizontal implemented

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,10 +4,13 @@ import { auth } from "../firebase/firebaseConfig";
 
 function Dashboard() {
     const user = auth.currentUser;
+
     return (
-        <Layout user = {user}>
+        <Layout user={user}>
             <h2 className="text-2xl font-bold text-blue-700 mb-4">Bem-vindo</h2>
-            <p className="text-gray-700">Escolha uma seção no menu para começar seus estudos.</p>
+            <p className="text-gray-700">
+                Escolha uma seção no menu para começar seus estudos.
+            </p>
         </Layout>
     );
 }
