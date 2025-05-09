a234d0530b37b393ac1f211bdd8ccc3986a3cdfe
brjatoba92 <brjatoba92@gmail.com>
2025-05-08 21:44:25 -0300

    Change Avatar User for initial name User

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Header({ user }) {
+  const getInitial = (name) => {
+    return name ? name.charAt(0).toUpperCase() : "U";
+  }
   return (
     <header className="bg-blue-700 text-white py-4 px-6 flex justify-between items-center shadow">
       <h1 className="text-xl font-bold">
@@ -14,15 +17,11 @@ function Header({ user }) {
             <p>Olá, {user.displayName || "Usuário"}</p>
             <p className="text-gray-200">{user.email}</p>
           </div>
-          <div>
-            <Link to="/perfil">
-                <img
-                    src={user.photoURL || "https://via.placeholder.com/40"}
-                    alt="Avatar"
-                    className="w-10 h-10 rounded-full object-cover border-2 border-white hover:scale-105 transition-transform"
-                />
-            </Link>
-          </div>
+          <Link to="/dashboard">
+            <div className="w-10 h-10 rounded-full bg-white text-blue-700 font-bold flex items-center justify-center border-2 border-white hover:scale-105 transition-transform">
+              {getInitial(user.displayName)}
+            </div>
+          </Link>
           
         </div>
       )}
